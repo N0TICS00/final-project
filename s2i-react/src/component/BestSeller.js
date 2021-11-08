2fc68a70e4ec91abd93a46308bf70a794a4910b6
N0TICS0 <74853755+N0TICS0@users.noreply.github.com>
2021-11-08 21:14:56 +0100

    Third commit, fetched NYT api

--- s2i-react/src/component/BestSeller.js
+++ s2i-react/src/component/BestSeller.js
@@ -1,21 +1,32 @@
 import axios from "axios"
 import { apiKeyNYT } from "../api"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 const BestSeller = () => {
     const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${apiKeyNYT}`
     const [bestSellers , setBestSellers] = useState([])
     const getBestSellers = async () => {
         const result = await axios.get(url)
         const data = result.data.results.books
-        console.log(data)
-        data.map((book) => {
-            setBestSellers([{}])
-        })
+        setBestSellers(data)
     }
-    getBestSellers()
+    useEffect(() => {
+        getBestSellers()
+    } , [])
     return(
         <>
-        <div className="best-seller">Ciao</div>
+        <div className="best-seller">
+            {
+                bestSellers.map((bestSeller) => {
+                    const {author , title } = bestSeller
+                    return(
+                        <div key={new Date().getDate().toString()}>
+                            <h1>{author}</h1>
+                            <h2>{title}</h2>
+                        </div>
+                    )
+                })
+            }
+        </div>
         </>
     )
 }
