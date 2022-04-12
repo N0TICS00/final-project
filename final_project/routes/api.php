3fa00d41d6e50e3a774d21d534cccf408abeae65
N0TICS0 <74853755+N0TICS0@users.noreply.github.com>
2022-04-12 21:01:43 +0200

    Added Users and Courses Controller, added API's routes

--- final_project/routes/api.php
+++ final_project/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Course;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,4 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('courses',);
+//COURSES API
+Route::get('/courses', function(){
+    return response(Course::All(), 200);
+});
+Route::get('/courses/{id}', function($id){
+    return Course::where('id', $id)->first();
+});
+Route::delete('/courses/{id}', function($id){
+    Course::find($id)->delete();
+    return 204;
+});
+//USERS API
+Route::get('/users', function(){
+    return response(User::All(), 200);
+});
+Route::get('/users/{id}', function($id){
+    return User::where('id', $id)->first();
+});
+Route::post('/users', function(Request $request){
+    $resp = User::create($request->all());
+    $resp->password= Hash::make($request->password);
+    return $resp;
+});
+
