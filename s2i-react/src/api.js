978ac4a06802fc6c637b05d5c8f66daac09e9873
N0TICS0 <76558194+N0TICS00@users.noreply.github.com>
2022-07-03 16:19:54 +0200

    Delete s2i-react directory

--- s2i-react/src/api.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    'apiKeyGoogle' : "AIzaSyAcG82vPTXSSuK7w7uOQCTkTNla_jmpI6Y",
-    'apiKeyNYT' : "xWzmdoZS0sYGznggekTAcAZU6QYXdaFb"
-
-};
--- s2i-react/src/component/Footer.js
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-    return(
-        <div className="footer">
-            <h1>Made with ❤️ by <a className="footer-a" href="https://n0tics00.github.io/Start2Impact_HTML_CSS/">Paolo D'Agostino</a></h1>
-        </div>
-    )
-}
-export default Footer
